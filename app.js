c8065f84cd6985b5a501be678a95b12830cc1881
DrewWeth <dgwetherington@gmail.com>
2015-03-04 03:02:44 -0600

    node added

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

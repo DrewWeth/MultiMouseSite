0d06d8174b79eb66a63ca2b6a75350e4b38b4669
DrewWeth <dgwetherington@gmail.com>
2015-03-04 14:59:56 -0600

    updates

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/support', function(req, res){
+  res.render('support');
+});
 
 
 http.listen(process.env.PORT || 3000, function(){
